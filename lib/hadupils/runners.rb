5767b7398c27f5d400c93ea4f007a142cc0d3d69
Ethan Rowe <ethan@the-rowes.com>
2013-09-09 17:59:07 -0400

    Hive runner/command support HIVE_AUX_JARS_PATH
    The hive runner and command need to be able to support the
    specification of HIVE_AUX_JARS_PATH in the underlying hive
    environment at call time, based on hive extension information.
    
    This nastiness involved:
    * Adding support to the command runner to detect the case where the
      first parameter to be executed and handle it special (for pre
      ruby-1.9)
    * Adding support to the runner for inclusion of the hive auxiliary
      jars into the mix (as HIVE_AUX_JARS_PATH environment variable)
    * Various tests that mess with globals like ::ENV and ::RUBY_VERSION.
    
    Fortunately, this all means that if you have hive extensions
    following established conventions, their auxiliary jars should be
    in place within the environment variable.

--- lib/hadupils/runners.rb
+++ lib/hadupils/runners.rb
@@ -8,8 +8,32 @@ module Hadupils::Runners
 
     def command; end
 
+    def execute!
+      command_list = command
+      if RUBY_VERSION < '1.9' and command_list[0].kind_of? Hash
+        deletes = []
+        overrides = {}
+        begin
+          command_list[0].each do |key, val|
+            if ::ENV.has_key? key
+              overrides[key] = ::ENV[key]
+            else
+              deletes << key
+            end
+            ::ENV[key] = val
+          end
+          Kernel.system(*command_list[1..-1])
+        ensure
+          overrides.each {|key, val| ::ENV[key] = val }
+          deletes.each {|key| ::ENV.delete key }
+        end
+      else
+        Kernel.system(*command_list)
+      end
+    end
+
     def wait!
-      @last_result = Kernel.system(*command)
+      @last_result = execute!
       @last_status = $?
       if @last_result.nil?
         255
@@ -18,12 +42,17 @@ module Hadupils::Runners
       end
     end
 
-    def self.run(params)
-      self.new(params).wait!
+    def self.run(*params)
+      self.new(*params).wait!
     end
   end
 
   class Hive < Base
+    def initialize(params, hive_aux_jars_path='')
+      super(params)
+      @hive_aux_jars_path = hive_aux_jars_path
+    end
+
     def self.base_runner
       @base_runner || ::File.join(ENV['HIVE_HOME'], 'bin', 'hive')
     end
@@ -33,13 +62,24 @@ module Hadupils::Runners
     end
 
     def command
-      items = params.inject([self.class.base_runner]) do |result, param|
+      params.inject([env_overrides, self.class.base_runner]) do |result, param|
         if param.respond_to? :hive_opts
           result + param.hive_opts
         else
           result << param
         end
       end
+    end
+
+    def env_overrides
+      e = {}
+      settings = [@hive_aux_jars_path, ::ENV['HIVE_AUX_JARS_PATH']].reject do |val|
+        val.nil? or val.strip == ''
+      end
+      if settings.length > 0
+        e['HIVE_AUX_JARS_PATH'] = settings.join(',')
+      end
+      e
     end
   end
 end
